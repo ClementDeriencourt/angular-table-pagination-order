027732acf9b646ed100acb1b67fbd70fd2a469b0
Clément Deriencourt <clement-deriencourt@orange.fr>
2020-09-24 10:33:35 +0200

    Table w/ pagination && choose order for id/name/weight/symbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablePaginationComponent } from './table-pagination/table-pagination.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator'; //a ne pas oublier pour que la fonctionnalité soit active !!
+import {CdkTableModule} from '@angular/cdk/table';
+import {MatSort, MatSortHeader, MatSortModule} from '@angular/material/sort'; //a ne pas oublier pour que la fonctionnalité soit active !!
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablePaginationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    CdkTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
